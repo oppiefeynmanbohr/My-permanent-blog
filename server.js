ab54691a177e3da67dbfd64e86be8f3644b5956f
oppiefeynmanbohr <oppiefeynmanbohr@gmail.com>
2026-07-26 07:35:22 +0000

    Disable caching for page1 and app.js

--- server.js
+++ server.js
@@ -398,6 +398,14 @@ app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 app.use(loadUserSession);
 
+app.get('/app.js', (req, res) => {
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  res.setHeader('Surrogate-Control', 'no-store');
+  res.sendFile(path.join(__dirname, 'public', 'app.js'));
+});
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -1526,6 +1534,10 @@ app.post('/api/admin/sms/verify', (req, res) => {
 });
 
 app.get(['/', '/page1'], (req, res) => {
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  res.setHeader('Surrogate-Control', 'no-store');
   res.sendFile(path.join(__dirname, 'public', 'page1.html'));
 });
 
